a4aa82b67df16d0abf11d40ce817d988a4b34ee9
themylogin <themylogin@gmail.com>
2013-01-20 19:09:09 +0700

    new comments not being bold

--- block/last_comments.py
+++ block/last_comments.py
@@ -14,8 +14,8 @@ def block(request, limit=10):
                 "comment"       : db.query(Comment).options(joinedload("content_item")).filter(Comment.content_item == comment.content_item).order_by(-Comment.created_at).first(),
                 "content_item"  : process_content_item(comment.content_item),
 
-                "count_new"     : db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at > request.user.last_activity).scalar() if request.user else 0,
-                "new_index"     : db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at <= request.user.last_activity).scalar() if request.user else 0,
+                "count_new"     : db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at > request.user.last_visit).scalar() if request.user else 0,
+                "new_index"     : db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at <= request.user.last_visit).scalar() if request.user else 0,
             }
             for comment in db.query(Comment).group_by(Comment.content_item_id).order_by(-func.max(Comment.created_at))[:limit]
         ],
