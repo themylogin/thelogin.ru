e78b2d89b4d349db2c82a221ba77e718410cfcba
themylogin <themylogin@gmail.com>
2013-01-21 17:22:34 +0700

    give theContentLoader load* methods a return value

--- www-root/asset/js/10-theContentLoader.js
+++ www-root/asset/js/10-theContentLoader.js
@@ -14,94 +14,89 @@
         },
 
         loadUp: function(count, eventHandlers){
-            return this.each(function(){
-                var $contentArea = $(this);
-                var settings = $(this).data("theContentLoader");
+            var $contentArea = $(this);
+            var settings = $(this).data("theContentLoader");
 
-                var query = {
-                    "count" : count,
-                    "after" : $contentArea.is(":parent") ? $contentArea.find(">:first").data("createdAt") : undefined,
-                };
-                var newEventHandlers = $.extend({
-                    "load"          : undefined,
-                    "contentItem"   : function(contentItem){
-                        $contentArea.prepend(contentItem);
-                    },
-                    "ready"         : undefined,
-                }, eventHandlers);
-                $(this).theContentLoader("_load", "theContentLoader_nothingUpAt", query, newEventHandlers);
-            });
+            var query = {
+                "count" : count,
+                "after" : $contentArea.is(":parent") ? $contentArea.find(">:first").data("createdAt") : undefined,
+            };
+            var newEventHandlers = $.extend({
+                "load"          : undefined,
+                "contentItem"   : function(contentItem){
+                    $contentArea.prepend(contentItem);
+                },
+                "ready"         : undefined,
+            }, eventHandlers);
+            return $(this).theContentLoader("_load", "theContentLoader_nothingUpAt", query, newEventHandlers);
         },
         loadDown: function(count, eventHandlers){
-            return this.each(function(){
-                var $contentArea = $(this);
-                var settings = $(this).data("theContentLoader");
+            var $contentArea = $(this);
+            var settings = $(this).data("theContentLoader");
 
-                var query = {
-                    "count" : count,
-                    "before": $contentArea.is(":parent") ? $contentArea.find(">:last").data("createdAt") : $contentArea.data("start"),
-                };
-                var newEventHandlers = $.extend({
-                    "load"          : undefined,
-                    "contentItem"   : function(contentItem){
-                        $contentArea.append(contentItem);
-                    },
-                    "ready"         : undefined,
-                }, eventHandlers);
-                $(this).theContentLoader("_load", "theContentLoader_nothingDownAt", query, newEventHandlers);
-            });
+            var query = {
+                "count" : count,
+                "before": $contentArea.is(":parent") ? $contentArea.find(">:last").data("createdAt") : $contentArea.data("start"),
+            };
+            var newEventHandlers = $.extend({
+                "load"          : undefined,
+                "contentItem"   : function(contentItem){
+                    $contentArea.append(contentItem);
+                },
+                "ready"         : undefined,
+            }, eventHandlers);
+            return $(this).theContentLoader("_load", "theContentLoader_nothingDownAt", query, newEventHandlers);
         },
         _load: function(timeoutKey, query, eventHandlers){
-            return this.each(function(){
-                var $contentArea = $(this);
-                var settings = $(this).data("theContentLoader");
+            var $contentArea = $(this);
+            var settings = $(this).data("theContentLoader");
 
-                if (new Date().getTime() < $(this).data(timeoutKey) + settings["updateTimeout"])
+            if (new Date().getTime() < $(this).data(timeoutKey) + settings["updateTimeout"])
+            {
+                return false;
+            }
+
+            if ($contentArea.hasClass("loading"))
+            {
+                return false;
+            }
+
+            $contentArea.addClass("loading");
+            $.ajax({
+                url     :   (settings["feed"] == "index" ? "" : "/" + settings["feed"]) + "/json/",
+                data    :   query,
+                dataType:   "json",
+                success :   function(contentItems)
                 {
-                    return;
-                }
-
-                if ($contentArea.hasClass("loading"))
-                {
-                    return;
-                }
-
-                $contentArea.addClass("loading");
-                $.ajax({
-                    url     :   (settings["feed"] == "index" ? "" : "/" + settings["feed"]) + "/json/",
-                    data    :   query,
-                    dataType:   "json",
-                    success :   function(contentItems)
+                    if (!contentItems.length)
                     {
-                        if (!contentItems.length)
-                        {
-                            $contentArea.data(timeoutKey, new Date().getTime());
-                        }
-
-                        if (eventHandlers["load"])
-                        {
-                            eventHandlers["load"]();
-                        }
-                        $.each(contentItems, function(i, contentItem){
-                            if (eventHandlers["contentItem"])
-                            {
-                                eventHandlers["contentItem"](contentItem);
-                            }
-                        });
-                        if (eventHandlers["ready"])
-                        {
-                            eventHandlers["ready"]();
-                        }
-
-                        $contentArea.removeClass("loading");
-                        $(window).trigger("theContentLoaded");
-                    },
-                    error   :   function()
-                    {
-                        $contentArea.removeClass("loading");
+                        $contentArea.data(timeoutKey, new Date().getTime());
                     }
-                });
+
+                    if (eventHandlers["load"])
+                    {
+                        eventHandlers["load"]();
+                    }
+                    $.each(contentItems, function(i, contentItem){
+                        if (eventHandlers["contentItem"])
+                        {
+                            eventHandlers["contentItem"](contentItem);
+                        }
+                    });
+                    if (eventHandlers["ready"])
+                    {
+                        eventHandlers["ready"]();
+                    }
+
+                    $contentArea.removeClass("loading");
+                    $(window).trigger("theContentLoaded");
+                },
+                error   :   function()
+                {
+                    $contentArea.removeClass("loading");
+                }
             });
+            return true;
         },
     };
 
