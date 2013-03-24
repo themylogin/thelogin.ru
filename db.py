0c9c63e78260b4db8d83736ab0e6fec36646779e
themylogin <themylogin@gmail.com>
2013-03-24 13:56:39 +0700

    pessimistic db disconnection handling

--- db.py
+++ db.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from sqlalchemy import create_engine, event, exc
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import create_session, scoped_session
+from sqlalchemy.pool import Pool
+
 from config import config
 from local import local_manager
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import create_session, scoped_session
-
 db = scoped_session(lambda: create_session(create_engine(config.db, echo=config.debug)), local_manager.get_ident)
 
-from sqlalchemy.ext.declarative import declarative_base
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
+
 Base = declarative_base()
