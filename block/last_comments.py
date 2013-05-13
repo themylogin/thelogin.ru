c5aa3b557018892cc245d7cfafc5046e3a8c67c2
themylogin <themylogin@gmail.com>
2013-05-13 21:57:57 +0700

    Serg @ReDetection Прямо сейчас твой блог говорит, что к фотке 553 есть новый коммент от анонимуса, но ссылка ведёт на первый, т.е. твой. и не сбрасывается

--- block/last_comments.py
+++ block/last_comments.py
@@ -15,7 +15,7 @@ def block(request, limit=10):
                 "content_item"  : process_content_item(comment.content_item),
 
                 "count_new"     : db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at > request.user.last_visit).scalar() if request.user else 0,
-                "new_index"     : db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at <= request.user.last_visit).scalar() if request.user else 0,
+                "new_index"     : (db.query(func.count(Comment)).filter(Comment.content_item == comment.content_item, Comment.created_at <= request.user.last_visit).scalar() if request.user else 0) + 1,
             }
             for comment in db.query(Comment).group_by(Comment.content_item_id).order_by(-func.max(Comment.created_at))[:limit]
         ],
