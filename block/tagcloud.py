4236fc086a9b0f415eb525cce86b8eec508599c2
themylogin <themylogin@gmail.com>
2013-05-26 16:03:56 +0700

    block/tagcloud.py PEP8

--- block/tagcloud.py
+++ block/tagcloud.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def block(feed):
-    from controller.content import get_content_feeds
+import operator
+from sqlalchemy import func
 
+from controller.content import get_content_feeds
+from controller.content.model import ContentItem, Tag
+from db import db
+
+def block(feed):
     if get_content_feeds()[feed]["url"] == "":
         feed_url = ""
     else:
         feed_url = "/" + get_content_feeds()[feed]["url"]
-
-    import operator
-    from db import db
-    from sqlalchemy import func
-    from controller.content.model import ContentItem, Tag
 
     tags = db.query(Tag, func.count(ContentItem)).join((ContentItem, Tag.content_items)).filter(ContentItem.type.in_(get_content_feeds()[feed]["types"])).group_by(Tag).order_by(Tag.title).all()
     if tags:
