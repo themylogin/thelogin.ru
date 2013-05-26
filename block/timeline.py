de0c03e00eac752d2727cf326d8eb1617da19d6d
themylogin <themylogin@gmail.com>
2013-05-26 16:04:43 +0700

    block/timeline.py PEP8

--- block/timeline.py
+++ block/timeline.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+from sqlalchemy import func
+
+from db import db
+from controller.content import get_content_feeds
+from controller.content.model import ContentItem
+
 def block(request, item=None):
     if item is not None:
         start = item["created_at"]
     else:
-        from datetime import datetime
         start = datetime.now()
-
-    from db import db    
-    from sqlalchemy import func
-    from controller.content import get_content_feeds
-    from controller.content.model import ContentItem
+    
     if db.query(func.count(ContentItem)).filter(ContentItem.type.in_(get_content_feeds()["timeline"]["types"]), ContentItem.created_at <= start).scalar() > 0:
         return {
             "start" : start,
