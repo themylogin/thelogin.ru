2c3655b2594d6ab83ee347790ff6f31526978ce0
themylogin <themylogin@gmail.com>
2013-05-26 16:50:32 +0700

    social_service/lastfm/__init__.py PEP8

--- social_service/lastfm/__init__.py
+++ social_service/lastfm/__init__.py
@@ -1,42 +1,48 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+import os
+import paste.deploy
+import pylast
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import create_session, scoped_session
+import sys
+from werkzeug.utils import redirect
+
+from config import config
+from kv import storage as kv_storage
+from local import local_manager
+from log import logger
+from threading import Thread
+from utils import urlencode
+
 class LastFM:	
-    def __init__(self, api_key, api_secret, username, thelogin_path=None):        
-        import pylast
+    def __init__(self, api_key, api_secret, username, thelogin_path=None):
         self.api_key = api_key
         self.api_secret = api_secret
         self.network = pylast.LastFMNetwork(self.api_key, self.api_secret)
 
         self.username = username
 
-        import sys
         sys.path.append(thelogin_path)
         from lastfm.models import User, Scrobble
         sys.path.pop()
         self.thelogin_User = User
         self.thelogin_Scrobble = Scrobble
-        import os.path
-        import paste.deploy
-        from sqlalchemy import engine_from_config
         engine = engine_from_config(paste.deploy.appconfig("config://" + os.path.join(thelogin_path, "production.ini")), 'sqlalchemy.')
-        from local import local_manager
-        from sqlalchemy.orm import create_session, scoped_session
         self.thelogin_db = scoped_session(lambda: create_session(engine), local_manager.get_ident)
         self.thelogin_user = self.thelogin_db.query(self.thelogin_User).filter(self.thelogin_User.username == self.username)[0]
 
     #
     def oauth_initiate(self, callback_url):
-        from werkzeug.utils import redirect
-        from utils import urlencode
         return redirect("%(homepage)s/api/auth/?api_key=%(api)s&cb=%(callback_url)s" % {
             "homepage"      : self.network.homepage,
             "api"           : self.api_key,
             "callback_url"  : urlencode(callback_url),
         })
 
-    def oauth_callback(self, request):
-        import pylast
+    def oauth_callback(self, request):        
         sg = pylast.SessionKeyGenerator(self.network)
         sg.web_auth_tokens["fake"] = request.args.get("token")
         sk = sg.get_web_auth_session_key("fake")
@@ -64,9 +70,6 @@ class LastFM:
         }
 
     def get_artist_image_at(self, artist, date):
-        from kv import storage as kv_storage
-        from datetime import datetime
-
         kv_key = artist + " @ " + date.strftime("%Y-%m-%d")
         try:
             return kv_storage["last.fm artist image"][kv_key]
@@ -92,13 +95,11 @@ class LastFM:
                             if best_prev_timedelta is None or best_prev_timedelta > timedelta:
                                 best_prev_timedelta = timedelta
                                 best_prev_timedelta_v = v
-                if best_prev_timedelta:
-                    from log import logger
+                if best_prev_timedelta:                    
                     logger.info("No last.fm artist '%s' image found at %s, copying previous image with timedelta %s", artist, date.strftime("%Y-%m-%d"), str(best_prev_timedelta))
                     kv_storage["last.fm artist image"][kv_key] = best_prev_timedelta_v
                     return kv_storage["last.fm artist image"][kv_key]
                 if best_timedelta:
-                    from log import logger
                     logger.info("No last.fm artist '%s' image found at %s, copying image with timedelta %s", artist, date.strftime("%Y-%m-%d"), str(best_timedelta_v))
                     kv_storage["last.fm artist image"][kv_key] = best_timedelta_v
                     return kv_storage["last.fm artist image"][kv_key]
@@ -107,7 +108,5 @@ class LastFM:
             def get_image_for_now():
                 kv_storage["last.fm artist image"][kv_key] = network.get_artist(artist).get_cover_image()
 
-            from threading import Thread
-            Thread(target=get_image_for_now).start()
-            from config import config
+            Thread(target=get_image_for_now).start()            
             return config.url + "/asset/img/social_service/last.fm/none.png"
