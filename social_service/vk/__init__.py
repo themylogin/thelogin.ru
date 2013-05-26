f63e9c8f458a61b9c3552b4351d0381f83e04b56
themylogin <themylogin@gmail.com>
2013-05-26 16:52:50 +0700

    social_service/vk/__init__.py PEP8

--- social_service/vk/__init__.py
+++ social_service/vk/__init__.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import simplejson
+import urllib2
+from werkzeug.utils import redirect
+
+from utils import urlencode
+
 class Vk:	
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
 
     def oauth_initiate(self, callback_url):
-        from utils import urlencode
-        from werkzeug.utils import redirect
         return redirect("http://api.vk.com/oauth/authorize?client_id=" + self.client_id + "&redirect_uri=" + urlencode(callback_url) + "&display=page")
 
     def oauth_callback(self, request):
@@ -16,8 +20,6 @@ class Vk:
             return False
 
         # TODO: import vkontakte
-        from utils import urlencode
-        import simplejson, urllib2
         access_token = simplejson.loads(urllib2.urlopen(urllib2.Request("https://api.vkontakte.ru/oauth/access_token?client_id=" + self.client_id + "&client_secret=" + self.client_secret + "&code=" + urlencode(request.args.get("code")))).read())
         user = simplejson.loads(urllib2.urlopen(urllib2.Request("https://api.vkontakte.ru/method/getProfiles?uid=" + str(access_token["user_id"]) + "&access_token=" + access_token["access_token"] + "&fields=uid,first_name,last_name,nickname,screen_name,photo")).read())["response"][0]
         
