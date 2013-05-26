5427f960dc4c27342e360205943407b8343c4f10
themylogin <themylogin@gmail.com>
2013-05-26 16:17:14 +0700

    controller/content/comment_text_processor.py PEP8

--- controller/content/comment_text_processor.py
+++ controller/content/comment_text_processor.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup
+from lxml.etree import tostring
+from lxml.html.soupparser import fromstring
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+import re
+from werkzeug.utils import escape
+
 from config import config
 
 def vidog(text):
     return text.replace("\r", "")
 
 def source(text):
-    import re
-
     text = re.sub("\s+<source", "<source", text)
     text = re.sub("</source>\s+", "</source>", text)
     text = text.replace("<source>\n", "<source>")
@@ -22,16 +29,12 @@ def source(text):
     return text
 
 def quote(text):
-    import re
-
     text = re.sub("\s+<quote>", "<quote>", text)
     text = re.sub("</quote>\s+", "</quote>", text)
     text = text.replace("<quote>", "<blockquote>").replace("</quote>", "</blockquote>")
     return text
 
 def newlines(text):
-    import re
-
     text = text.replace("\n", "<br />\n")
     # Not inside <source>!
     r = re.compile("<source(.*?)<br />(.*?)</source>", flags=re.DOTALL)
@@ -43,13 +46,8 @@ def newlines(text):
     return text
 
 def html_tags(text):
-    import re
-
-    from lxml.html.soupparser import fromstring
-    from lxml.etree import tostring
     text = tostring(fromstring(text), pretty_print=True, encoding="utf-8")
-
-    from bs4 import BeautifulSoup
+    
     soup = BeautifulSoup(text)
     for tag in soup.find_all():
         if tag.name not in ["html", "body", "p", "br", "b", "i", "s", "u", "var", "a", "img", "source", "pre", "blockquote"]:
@@ -77,9 +75,6 @@ def html_tags(text):
         # <source>
         if tag.name == "source":
             try:
-                from pygments import highlight
-                from pygments.lexers import get_lexer_by_name
-                from pygments.formatters import HtmlFormatter
                 tag.replace_with(BeautifulSoup(highlight(u''.join([unicode(i).replace("&lt;", "<").replace("&gt;", ">") for i in tag.contents]), get_lexer_by_name(tag["lang"], stripall=True, startinline=True), HtmlFormatter(cssclass="syntax"))))
             except:
                 tag.replace_with(BeautifulSoup(u'<pre>' + u''.join([unicode(i).replace("&lt;", "<").replace("&gt;", ">") for i in tag.contents]) + u'</pre>'))
@@ -91,8 +86,6 @@ def html_tags(text):
     text_old = ""
     while len(text_old) != len(text):
         text_old = text
-
-        import re
         text = text.strip()
         text = re.sub("^<br />", "", text)
         text = re.sub("<br />$", "", text)
@@ -100,7 +93,6 @@ def html_tags(text):
     return text
 
 def smilies(text):
-    from werkzeug.utils import escape
     for smilie in config.smilies:
         text = text.replace(escape(smilie), '<img class="smilie" src="' + config.url + config.smilies[smilie] + '" />')
     return text
