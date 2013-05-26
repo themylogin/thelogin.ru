ac739d6e88036372f21398d16f3edb44c3fbc365
themylogin <themylogin@gmail.com>
2013-05-26 15:45:29 +0700

    block/content_item_next_prev.py PEP8

--- block/content_item_next_prev.py
+++ block/content_item_next_prev.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def block(request, feed, item):
-    from db import db
-    from controller.content.model import ContentItem
-    from controller.content import get_content_feeds, process_content_item
-    
+from db import db
+from controller.content.model import ContentItem
+from controller.content import get_content_feeds, process_content_item
+
+def block(request, feed, item):    
     next = db.query(ContentItem).filter(ContentItem.type.in_(get_content_feeds()[feed]["types"]), ContentItem.permissions_for(request.user), ContentItem.created_at > item["created_at"]).order_by(ContentItem.created_at).first()
     prev = db.query(ContentItem).filter(ContentItem.type.in_(get_content_feeds()[feed]["types"]), ContentItem.permissions_for(request.user), ContentItem.created_at < item["created_at"]).order_by(ContentItem.created_at.desc()).first()
     if next or prev:
